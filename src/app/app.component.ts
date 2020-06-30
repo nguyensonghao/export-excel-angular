5219444476c94f7dfcffcebd1e3b9953c687f1e2
nguyensonghao94 <20121630@student.hut.edu.vn>
2020-06-30 21:23:44 +0700

    Create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import * as XLSX from 'xlsx'; 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-dream-app';
+  
+  export() {
+    let element = document.getElementById('table'); 
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'test.xls');
+  }
 }
